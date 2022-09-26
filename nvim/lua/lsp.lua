288a5140ac0a16820d01ecc79dc77d7b14de972d
Чернышев Евгений Михайлович <emchernyshev@domclick.ru>
2022-09-26 18:06:51 +0300

    Правка маппинга и конфига lspsaga

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,23 +1,26 @@
+local map = require('utils').map
+
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   -- Mappings.
-  local opts = { noremap=true, silent=true }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-  vim.keymap.set('n', 'lwa', vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set('n', 'lwr', vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set('n', 'lwl', function()
+  local opts = { silent = true, buffer = bufnr }
+  map('n', 'gD', vim.lsp.buf.declaration, opts)
+  map('n', 'gd', vim.lsp.buf.definition, opts)
+  map('n', 'K', vim.lsp.buf.hover, opts)
+  map('n', 'gi', vim.lsp.buf.implementation, opts)
+  map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  map('n', 'lwa', vim.lsp.buf.add_workspace_folder, opts)
+  map('n', 'lwr', vim.lsp.buf.remove_workspace_folder, opts)
+  map('n', 'lwl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, opts)
-  vim.keymap.set('n', 'lD', vim.lsp.buf.type_definition, opts)
-  vim.keymap.set('n', 'lrn', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', 'lca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', 'lf', vim.lsp.buf.formatting, opts)  --...
+  map('n', 'lD', vim.lsp.buf.type_definition, opts)
+  map('n', 'lrn', vim.lsp.buf.rename, opts)
+  map('n', 'lca', vim.lsp.buf.code_action, opts)
+  map('n', 'gr', vim.lsp.buf.references, opts)
+  map('n', 'lf', vim.lsp.buf.formatting, opts)  --...
 end
 
 
--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,30 +1,32 @@
+local map = require('utils').map
+
 -- common mappings
-vim.keymap.set('n', '<C-p>', ':GFiles<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>c', ':Buffers<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>x', ':Ag ', { noremap = true })
-vim.keymap.set('n', '<Leader>z', ':Rg ', { noremap = true })
-vim.keymap.set('n', '<Leader>d', ':Bdelete<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>t', ':tabclose<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>T', ':tabnew<CR>', { noremap = true, silent = true })
-vim.keymap.set('v', '//', 'y/<C-R>"<CR>', { noremap = true })
-vim.keymap.set('n', '<F8>', ':TagbarToggle<CR>', { noremap = true, silent = true })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+map('n', '<C-p>', ':GFiles<CR>', { silent = true })
+map('n', '<Leader>c', ':Buffers<CR>', { silent = true })
+map('n', '<Leader>x', ':Ag ')
+map('n', '<Leader>z', ':Rg ')
+map('n', '<Leader>d', ':Bdelete<CR>', { silent = true })
+map('n', '<Leader>t', ':tabclose<CR>', { silent = true })
+map('n', '<Leader>T', ':tabnew<CR>', { silent = true })
+map('v', '//', 'y/<C-R>"<CR>')
+map('n', '<F8>', ':TagbarToggle<CR>', { silent = true })
+map('t', '<Esc>', '<C-\\><C-n>', { silent = true })
 vim.cmd([[
 nnoremap <silent><leader>X :Ag <C-R>=expand("<cword>")<CR><CR>
 ]])
 
 -- NERDTree mappings
-vim.keymap.set('n', 'n<C-d>', ':NERDTreeClose<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'n<C-f>', ':NERDTreeFind<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'n<C-n>', ':NERDTree<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'n<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+map('n', 'n<C-d>', ':NERDTreeClose<CR>', { silent = true })
+map('n', 'n<C-f>', ':NERDTreeFind<CR>', { silent = true })
+map('n', 'n<C-n>', ':NERDTree<CR>', { silent = true })
+map('n', 'n<C-t>', ':NERDTreeToggle<CR>', { silent = true })
 
-vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'gh', '<Cmd>Lspsaga lsp_finder<CR>', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-j>', ':Lspsaga diagnostic_jump_next<CR>', { noremap = true, silent = true })
+map('n', 'K', ':Lspsaga hover_doc<CR>', { silent = true })
+map('n', 'gh', '<Cmd>Lspsaga lsp_finder<CR>', { silent = true })
+map('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', { silent = true })
+map('n', '<C-j>', ':Lspsaga diagnostic_jump_next<CR>', { silent = true })
 
-vim.keymap.set('v', '<Leader>p', ':Neoformat<CR>', { noremap = true })
+map('v', '<Leader>p', ':Neoformat<CR>')
 
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+map('n', 'zR', require('ufo').openAllFolds)
+map('n', 'zM', require('ufo').closeAllFolds)
