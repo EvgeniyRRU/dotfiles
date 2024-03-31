5ef87758cad6c1106233e0d1933616983b2bd3aa
Чернышев Евгений Михайлович <emchernyshev@domclick.ru>
2024-03-31 18:32:52 +0300

    Fixed nvim opening freeze for /opt/homebrew/bin/nvim --noplugin opening

---
+++ .wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+config.color_scheme = 'Ocean Dark (Gogh)'
+config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 11.0
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+return config
